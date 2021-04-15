8408b7adb609a6e1b6bb24fc0b4f9d94942bd6cf
Armin Reichert <armin.reichert@web.de>
2021-04-15 08:16:32 +0200

    mvn and modules

---
+++ graph-search/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module de.amr.graph.pathfinder {
+
+	requires transitive de.amr.graph.core;
+	
+	exports de.amr.graph.pathfinder.api;
+	exports de.amr.graph.pathfinder.impl;
+	exports de.amr.graph.pathfinder.util;
+
+}
